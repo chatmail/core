b064d229222698bd8da16fb8b237b0d04da22aeb
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-03-25 16:22:57 +0100

    remove log

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -3206,7 +3206,6 @@ async fn get_parent_message(
     if let Some(field) = references {
         mids.append(&mut parse_message_ids(field));
     }
-    info!(context, "mids: {mids:?}");
     message::get_by_rfc724_mids(context, &mids).await
 }
 
