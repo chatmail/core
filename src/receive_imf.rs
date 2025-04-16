8c4fa30af795536b0c87bf5ed6feaace6a1e76dd
link2xt <link2xt@testrun.org>
2025-04-16 01:15:38 +0000

    fix: create past group members with Origin::Hidden

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -346,18 +346,9 @@ pub(crate) async fn receive_imf_inner(
         },
     )
     .await?;
-    let past_ids = add_or_lookup_contacts_by_address_list(
-        context,
-        &mime_parser.past_members,
-        if !mime_parser.incoming {
-            Origin::OutgoingTo
-        } else if incoming_origin.is_known() {
-            Origin::IncomingTo
-        } else {
-            Origin::IncomingUnknownTo
-        },
-    )
-    .await?;
+    let past_ids =
+        add_or_lookup_contacts_by_address_list(context, &mime_parser.past_members, Origin::Hidden)
+            .await?;
 
     update_verified_keys(context, &mut mime_parser, from_id).await?;
 
