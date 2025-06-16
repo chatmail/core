7658e91733e0abb686ba3d2a25a616d9894ec3ed
B. Petersen <r10s@b44t.com>
2025-06-16 14:52:47 +0200

    document msg_id for all call events

--- deltachat-ffi/deltachat.h
+++ deltachat-ffi/deltachat.h
@@ -6719,7 +6719,7 @@ void dc_event_unref(dc_event_t* event);
  * Otherwise, ringing should end on #DC_EVENT_CALL_ENDED
  * or #DC_EVENT_INCOMING_CALL_ACCEPTED
  *
- * @param data1 (int) msg_id ID of the info-message referring to the call,
+ * @param data1 (int) msg_id ID of the info-message referring to the call
  */
 #define DC_EVENT_INCOMING_CALL                            2550
 
@@ -6729,6 +6729,8 @@ void dc_event_unref(dc_event_t* event);
  *
  * The event is sent unconditionally when the corresponding message is received.
  * UI should only take action in case call UI was opened before, otherwise the event should be ignored.
+ *
+ * @param data1 (int) msg_id ID of the info-message referring to the call
  */
  #define DC_EVENT_INCOMING_CALL_ACCEPTED                  2560
 
@@ -6737,6 +6739,8 @@ void dc_event_unref(dc_event_t* event);
  *
  * The event is sent unconditionally when the corresponding message is received.
  * UI should only take action in case call UI was opened before, otherwise the event should be ignored.
+ *
+ * @param data1 (int) msg_id ID of the info-message referring to the call
  */
 #define DC_EVENT_OUTGOING_CALL_ACCEPTED                   2570
 
@@ -6745,6 +6749,8 @@ void dc_event_unref(dc_event_t* event);
  *
  * The event is sent unconditionally when the corresponding message is received.
  * UI should only take action in case call UI was opened before, otherwise the event should be ignored.
+ *
+ * @param data1 (int) msg_id ID of the info-message referring to the call
  */
 #define DC_EVENT_CALL_ENDED                               2580
 
