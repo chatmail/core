c1024e19f421976b7e7c38731e3b2902f2e1126f
B. Petersen <r10s@b44t.com>
2025-07-07 12:50:46 +0200

    make clippy happy

--- src/mimefactory.rs
+++ src/mimefactory.rs
@@ -3,7 +3,7 @@
 use std::collections::{BTreeSet, HashSet};
 use std::io::Cursor;
 
-use anyhow::{anyhow, Context as _, Result, bail, ensure};
+use anyhow::{Context as _, Result, anyhow, bail, ensure};
 use base64::Engine as _;
 use deltachat_contact_tools::sanitize_bidi_characters;
 use mail_builder::headers::HeaderType;
