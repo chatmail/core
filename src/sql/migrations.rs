8c6494fbb77c91f82eebd33bbf053c313094318c
Hocuri <hocuri@gmx.de>
2025-07-07 17:41:31 +0200

    WIP: Start with decryption, and a test for it. Next TODO: SQL table migartion.

--- src/sql/migrations.rs
+++ src/sql/migrations.rs
@@ -1247,6 +1247,15 @@ CREATE INDEX gossip_timestamp_index ON gossip_timestamp (chat_id, fingerprint);
         );
     }
 
+    inc_and_check(&mut migration_version, 133)?;
+    if dbversion < migration_version {
+        sql.execute_migration(
+            "CREATE TABLE symmetric_secrets(
+            chat_id INTEGER PRIMARY KEY NOT NULL,
+            symmetric_secret: ",
+        )
+    }
+
     let new_version = sql
         .get_raw_config_int(VERSION_CFG)
         .await?
