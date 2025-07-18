2ba2d873a54cb98eaf33e77453edb0cf6c3acfb9
Hocuri <hocuri@gmx.de>
2025-07-18 20:21:59 +0200

    test: Fix jsonrpc-python tests

--- deltachat-rpc-client/tests/test_something.py
+++ deltachat-rpc-client/tests/test_something.py
@@ -13,6 +13,7 @@ import pytest
 from deltachat_rpc_client import Contact, EventType, Message, events
 from deltachat_rpc_client.const import ChatType, DownloadState, MessageState
 from deltachat_rpc_client.rpc import JsonRpcError
+from deltachat_rpc_client.pytestplugin import E2EE_INFO_MSGS
 
 
 def test_system_info(rpc) -> None:
@@ -457,8 +458,8 @@ def test_wait_next_messages(acfactory) -> None:
         alice_chat_bot.send_text("Hello!")
 
         next_messages = next_messages_task.result()
-        assert len(next_messages) == 1
-        snapshot = next_messages[0].get_snapshot()
+        assert len(next_messages) == 1 + E2EE_INFO_MSGS
+        snapshot = next_messages[0 + E2EE_INFO_MSGS].get_snapshot()
         assert snapshot.text == "Hello!"
 
 
