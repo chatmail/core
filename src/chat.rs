34ebfbbf2bbc5dae2e271c45c96ebdd37b7d956d
Hocuri <hocuri@gmx.de>
2025-08-01 16:32:40 +0200

    fix: make test_broadcast work, return an error when trying to add manually add a contact to a broadcast list, don't have unpromoted broadcast lists, make basic multi-device, inviter side, work

--- src/chat.rs
+++ src/chat.rs
@@ -3794,8 +3794,8 @@ pub(crate) async fn create_broadcast_ex(
             }
             t.execute(
                 "INSERT INTO chats \
-                (type, name, grpid, param, created_timestamp) \
-                VALUES(?, ?, ?, \'U=1\', ?);",
+                (type, name, grpid, created_timestamp) \
+                VALUES(?, ?, ?, ?);",
                 (
                     Chattype::OutBroadcast,
                     &chat_name,
@@ -3971,8 +3971,8 @@ pub(crate) async fn add_contact_to_chat_ex(
     // this also makes sure, no contacts are added to special or normal chats
     let mut chat = Chat::load_from_db(context, chat_id).await?;
     ensure!(
-        chat.typ == Chattype::Group || chat.typ == Chattype::OutBroadcast,
-        "{} is not a group/broadcast where one can add members",
+        chat.typ == Chattype::Group,
+        "{} is not a group where one can add members",
         chat_id
     );
     ensure!(
@@ -3981,10 +3981,6 @@ pub(crate) async fn add_contact_to_chat_ex(
         contact_id
     );
     ensure!(!chat.is_mailing_list(), "Mailing lists can't be changed");
-    ensure!(
-        chat.typ != Chattype::OutBroadcast || contact_id != ContactId::SELF,
-        "Cannot add SELF to broadcast channel."
-    );
     ensure!(
         chat.is_encrypted(context).await? == contact.is_key_contact(),
         "Only key-contacts can be added to encrypted chats"
@@ -4036,7 +4032,7 @@ pub(crate) async fn add_contact_to_chat_ex(
         }
         add_to_chat_contacts_table(context, time(), chat_id, &[contact_id]).await?;
     }
-    if chat.typ == Chattype::Group && chat.is_promoted() {
+    if chat.is_promoted() {
         msg.viewtype = Viewtype::Text;
 
         let contact_addr = contact.get_addr().to_lowercase();
