7e4822c8cacd19966c819a6841aa6b5cbe28de5a
link2xt <link2xt@testrun.org>
2025-08-14 13:28:09 +0000

    fix: do not reverify already verified contacts via gossip
    If the contact is already introduced by someone,
    usually by adding to a verified group,
    it should not be reverified because of another
    chat message is a verified group.
    This usually results is verification loops
    and is not meaningful because the verifier
    likely got this same contact introduced
    in the same group.

--- src/contact.rs
+++ src/contact.rs
@@ -1977,8 +1977,9 @@ pub(crate) async fn mark_contact_id_as_verified(
                 }
             }
             transaction.execute(
-                "UPDATE contacts SET verifier=? WHERE id=?",
-                (verifier_id, contact_id),
+                "UPDATE contacts SET verifier=?1
+                 WHERE id=?2 AND (verifier=0 OR ?1=?3)",
+                (verifier_id, contact_id, ContactId::SELF),
             )?;
             Ok(())
         })
