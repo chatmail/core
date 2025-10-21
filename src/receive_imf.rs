24cbcbe9f7dfb3ad839301a1ab1dff0a7150ac51
Hocuri <hocuri@gmx.de>
2025-10-21 13:46:58 +0200

    fix: Don't add duplicate member-added message

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -3530,9 +3530,6 @@ async fn apply_in_broadcast_changes(
                 info!(context, "No-op broadcast 'Member added' message (TRASH)");
                 msg = "".to_string();
             } else {
-                chat.id
-                    .add_encrypted_msg(context, mime_parser.timestamp_sent)
-                    .await?;
                 msg = stock_str::msg_add_member_local(context, ContactId::SELF, from_id).await;
             }
 
