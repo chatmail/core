48b374326734bea49049a1b670ef1572e0440c46
holger krekel <holger@merlinux.eu>
2025-11-25 00:42:25 +0100

    give up on windows

--- deltachat-rpc-client/tests/test_rpc_virtual.py
+++ deltachat-rpc-client/tests/test_rpc_virtual.py
@@ -1,17 +1,17 @@
-import platform
 import subprocess
 import sys
+from platform import system  # noqa
+
+import pytest
 
 from deltachat_rpc_client import DeltaChat, Rpc
 
 
+@pytest.mark.skipif("system() == 'Windows'")
 def test_install_venv_and_use_other_core(tmp_path):
     venv = tmp_path.joinpath("venv1")
     subprocess.check_call([sys.executable, "-m", "venv", venv])
-    if platform.system() == "Windows":
-        python = venv / "Scripts" / "python.exe"
-    else:
-        python = venv / "bin" / "python"
+    python = venv / "bin" / "python"
     subprocess.check_call([python, "-m", "pip", "install", "deltachat-rpc-server==2.20.0"])
     rpc = Rpc(accounts_dir=tmp_path.joinpath("accounts"), rpc_server_path=venv.joinpath("bin", "deltachat-rpc-server"))
 
