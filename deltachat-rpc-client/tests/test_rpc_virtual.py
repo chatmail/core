941734ac2cfc56877d25ac72e8697b3e1e6288f5
holger krekel <holger@merlinux.eu>
2025-11-24 14:34:23 +0100

    try invoking pip indirectly to make windows CI pass

--- deltachat-rpc-client/tests/test_rpc_virtual.py
+++ deltachat-rpc-client/tests/test_rpc_virtual.py
@@ -6,9 +6,9 @@ from deltachat_rpc_client import DeltaChat, Rpc
 
 def test_install_venv_and_use_other_core(tmp_path):
     venv = tmp_path.joinpath("venv1")
-    python = sys.executable
-    subprocess.check_call([python, "-m", "venv", venv])
-    subprocess.check_call([venv / "bin" / "pip", "install", "deltachat-rpc-server==2.20.0"])
+    subprocess.check_call([sys.executable, "-m", "venv", venv])
+    python = venv / "bin" / "python"
+    subprocess.check_call([python, "-m", "pip", "install", "deltachat-rpc-server==2.20.0"])
     rpc = Rpc(accounts_dir=tmp_path.joinpath("accounts"), rpc_server_path=venv.joinpath("bin", "deltachat-rpc-server"))
 
     with rpc:
