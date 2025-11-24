94625d9c9c637881a50f616b5c45b0acf9e5d56c
holger krekel <holger@merlinux.eu>
2025-11-25 00:19:18 +0100

    attempt to make CI work on windows, and disable enforcement of ternary operator which makes too long lines

--- deltachat-rpc-client/tests/test_rpc_virtual.py
+++ deltachat-rpc-client/tests/test_rpc_virtual.py
@@ -1,3 +1,4 @@
+import platform
 import subprocess
 import sys
 
@@ -7,7 +8,10 @@ from deltachat_rpc_client import DeltaChat, Rpc
 def test_install_venv_and_use_other_core(tmp_path):
     venv = tmp_path.joinpath("venv1")
     subprocess.check_call([sys.executable, "-m", "venv", venv])
-    python = venv / "bin" / "python"
+    if platform.system() == "Windows":
+        python = venv / "Scripts" / "python.exe"
+    else:
+        python = venv / "bin" / "python"
     subprocess.check_call([python, "-m", "pip", "install", "deltachat-rpc-server==2.20.0"])
     rpc = Rpc(accounts_dir=tmp_path.joinpath("accounts"), rpc_server_path=venv.joinpath("bin", "deltachat-rpc-server"))
 
