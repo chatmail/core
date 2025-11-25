8a9d56bde82431e056c845d1c5a5dffd3b86b0ea
holger krekel <holger@merlinux.eu>
2025-11-25 23:46:44 +0100

    merge test_rpc_virtual into new cross-core testing file

--- deltachat-rpc-client/tests/test_cross_core.py
+++ deltachat-rpc-client/tests/test_cross_core.py
@@ -1,4 +1,19 @@
+import subprocess
+
 import pytest
+
+from deltachat_rpc_client import DeltaChat, Rpc
+
+
+def test_install_venv_and_use_other_core(tmp_path, get_core_python_env):
+    venv = get_core_python_env("2.20.0")
+    python = venv / "bin" / "python"
+    subprocess.check_call([python, "-m", "pip", "install", "deltachat-rpc-server==2.20.0"])
+    rpc = Rpc(accounts_dir=tmp_path.joinpath("accounts"), rpc_server_path=venv.joinpath("bin", "deltachat-rpc-server"))
+
+    with rpc:
+        dc = DeltaChat(rpc)
+        assert dc.rpc.get_system_info()["deltachat_core_version"] == "v2.20.0"
 
 
 @pytest.mark.parametrize("version", ["2.20.0", "2.10.0"])
