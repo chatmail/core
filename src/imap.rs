397de3461a6b46b73924e1767d3ab0041a55bf0e
iequidoo <dgreshilov@gmail.com>
2026-01-20 18:31:23 -0300

    Try removing imap::Session::sync_seen_flags()

--- src/imap.rs
+++ src/imap.rs
@@ -1205,6 +1205,7 @@ impl Session {
     }
 
     /// Synchronizes `\Seen` flags using `CONDSTORE` extension.
+    #[allow(dead_code)]
     pub(crate) async fn sync_seen_flags(&mut self, context: &Context, folder: &str) -> Result<()> {
         if !self.can_condstore() {
             info!(
