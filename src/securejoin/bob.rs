cca6bd4f191372bc0fd4f89e451cd80b9ecc9625
Hocuri <hocuri@gmx.de>
2026-02-10 15:37:04 +0100

    Remove TODO

--- src/securejoin/bob.rs
+++ src/securejoin/bob.rs
@@ -302,9 +302,7 @@ pub(crate) async fn send_handshake_message(
     step: BobHandshakeMsg,
 ) -> Result<()> {
     if invite.is_v3() && matches!(step, BobHandshakeMsg::Request) {
-        // TODO this code might be moved "up" into the caller function
         // Send a minimal symmetrically-encrypted vc-request message
-
         let rfc724_mid = create_outgoing_rfc724_mid();
         let contact = Contact::get_by_id(context, invite.contact_id()).await?;
         let recipient = contact.get_addr();
