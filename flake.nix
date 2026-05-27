1e3d4381e03544e5a21a9d181c8e1950150ab6b8
link2xt <link2xt@testrun.org>
2026-05-27 17:00:27 +0200

    refactor: remove support for building "source" packages for deltachat-rpc-server
    This hack was needed to "build" Android packages
    by repackaging Linux packages on-the-fly,
    but PyPI has since added Android support
    and we are using it directly.

--- flake.nix
+++ flake.nix
@@ -518,22 +518,6 @@
                 '';
               };
 
-            # Source package for deltachat-rpc-server.
-            # Fake package that downloads Linux version,
-            # needed to install deltachat-rpc-server on Android with `pip`.
-            deltachat-rpc-server-source =
-              pkgs.stdenv.mkDerivation {
-                pname = "deltachat-rpc-server-source";
-                version = manifest.version;
-                src = pkgs.lib.cleanSource ./.;
-                nativeBuildInputs = [
-                  pkgs.python3
-                  pkgs.python3Packages.wheel
-                ];
-                buildPhase = ''python3 scripts/wheel-rpc-server.py source deltachat_rpc_server-${manifest.version}.tar.gz'';
-                installPhase = ''mkdir -p $out; cp -av deltachat_rpc_server-${manifest.version}.tar.gz $out'';
-              };
-
             deltachat-rpc-client =
               pkgs.python3Packages.buildPythonPackage {
                 pname = "deltachat-rpc-client";
