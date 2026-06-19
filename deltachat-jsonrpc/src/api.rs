0c064a52fc16aca42a24d64e97218b1c65a53dd4
d2weber <29163905+d2weber@users.noreply.github.com>
2026-06-19 18:39:33 +0200

    feat: generate jsonrpc headers at build time

--- deltachat-jsonrpc/src/api.rs
+++ deltachat-jsonrpc/src/api.rs
@@ -157,7 +157,7 @@ impl CommandApi {
     }
 }
 
-#[rpc(all_positional, ts_outdir = "typescript/generated")]
+#[rpc(all_positional)]
 impl CommandApi {
     /// Test function.
     async fn sleep(&self, delay: f64) {
