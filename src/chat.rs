preamoruk/epsilonchat

refactor(mimefactory): separate rendering of message payload and sendable message
This change separates rendering into two separate steps:
1. Rendering of the message payload without the From, Date and Autocrypt headers.
2. Adding the From, Date and Autocrypt headers and possibly encrypting the message.

The goal is to have serializable result of the first step
that can be persisted in the database and sent later with any email address.
This way it will be possible to send queued messages over any relay.

This will make it possible not to remove all messages from the queue
when the sending relay is changed.
Currently changing `configured_addr` deletes everything from `smtp` table.

This change is however only a refactoring and does not implement any features.

src/chat.rs
@@ -35,11 +35,13 @@ use crate::download::{
 use crate::ensure_and_debug_assert_eq;
 use crate::ephemeral::{Timer as EphemeralTimer, start_chat_ephemeral_timers};
 use crate::events::EventType;
+use crate::key;
 use crate::key::{Fingerprint, self_fingerprint};
 use crate::location;
 use crate::log::{LogExt, warn};
 use crate::logged_debug_assert;
 use crate::message::{self, Message, MessageState, MsgId, Viewtype};
+use crate::mimefactory;
 use crate::mimefactory::{MimeFactory, RenderedEmail};
 use crate::mimeparser::SystemMessage;
 use crate::param::{Param, Params};
@@ -2757,6 +2759,11 @@ async fn render_mime_message_and_pre_message(
     msg: &mut Message,
     mimefactory: MimeFactory,
 ) -> Result<(Option<RenderedEmail>, RenderedEmail)> {
+    let from_addr = context.get_primary_self_addr().await?;
+    let public_key = key::load_self_public_key(context).await?;
+    let secret_key = key::load_self_secret_key(context).await?;
+    let timestamp = msg.timestamp_sort;
+
     let needs_pre_message = msg.viewtype.has_file()
         && mimefactory.will_be_encrypted() // unencrypted is likely email, we don't want to spam by sending multiple messages
         && msg
@@ -2773,15 +2780,32 @@ async fn render_mime_message_and_pre_message(
 
         let mut mimefactory_post_msg = mimefactory.clone();
         mimefactory_post_msg.set_as_post_message();
-        let rendered_msg = Box::pin(mimefactory_post_msg.render(context))
+        let (queued_msg, side_effects) = Box::pin(mimefactory_post_msg.pre_render(context))
             .await
             .context("Failed to render post-message")?;
 
+        let rendered_msg = mimefactory::render_queued_mail(
+            queued_msg,
+            &public_key,
+            &secret_key,
+            from_addr.clone(),
+            timestamp,
+            side_effects,
+        )?;
+
         let mut mimefactory_pre_msg = mimefactory;
         mimefactory_pre_msg.set_as_pre_message_for(&rendered_msg);
-        let rendered_pre_msg = Box::pin(mimefactory_pre_msg.render(context))
+        let (queued_pre_msg, pre_side_effects) = Box::pin(mimefactory_pre_msg.pre_render(context))
             .await
             .context("pre-message failed to render")?;
+        let rendered_pre_msg = mimefactory::render_queued_mail(
+            queued_pre_msg,
+            &public_key,
+            &secret_key,
+            from_addr,
+            timestamp,
+            pre_side_effects,
+        )?;
 
         if rendered_pre_msg.message.len() > PRE_MSG_SIZE_WARNING_THRESHOLD {
             warn!(
@@ -2794,7 +2818,17 @@ async fn render_mime_message_and_pre_message(
 
         Ok((Some(rendered_pre_msg), rendered_msg))
     } else {
-        Ok((None, Box::pin(mimefactory.render(context)).await?))
+        let (queued_msg, side_effects) = Box::pin(mimefactory.pre_render(context)).await?;
+        let rendered_msg = mimefactory::render_queued_mail(
+            queued_msg,
+            &public_key,
+            &secret_key,
+            from_addr,
+            timestamp,
+            side_effects,
+        )?;
+
+        Ok((None, rendered_msg))
     }
 }
 
@@ -2839,7 +2873,6 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
             return Err(err);
         }
     };
-    let attach_selfavatar = mimefactory.attach_selfavatar;
     let mut recipients = mimefactory.recipients();
 
     let from = context.get_primary_self_addr().await?;
@@ -2926,14 +2959,22 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
 
     let now = time();
 
-    if let Some(last_added_location_timestamp) = rendered_msg.last_added_location_timestamp {
+    if let Some(last_added_location_timestamp) =
+        rendered_msg.side_effects.last_added_location_timestamp
+    {
         location::set_kml_sent_timestamp(context, msg.chat_id, last_added_location_timestamp)
             .await?;
     }
 
-    if attach_selfavatar && let Err(err) = msg.chat_id.set_selfavatar_timestamp(context, now).await
+    if rendered_msg.side_effects.avatar_is_attached
+        || rendered_pre_msg
+            .as_ref()
+            .is_some_and(|msg| msg.side_effects.avatar_is_attached)
     {
-        error!(context, "Failed to set selfavatar timestamp: {err:#}.");
+        msg.chat_id
+            .set_selfavatar_timestamp(context, now)
+            .await
+            .context("Failed to set selfavatar timestamp")?;
     }
 
     if rendered_msg.is_encrypted {
@@ -2941,7 +2982,7 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
     } else {
         msg.param.remove(Param::GuaranteeE2ee);
     }
-    msg.subject.clone_from(&rendered_msg.subject);
+    msg.subject.clone_from(&rendered_msg.side_effects.subject);
     // Sort the message to the bottom. Employ `msgs_index7` to compute `timestamp`.
     context
         .sql
@@ -2974,7 +3015,7 @@ WHERE id=?
     let trans_fn = |t: &mut rusqlite::Transaction| {
         let mut row_ids = Vec::<i64>::new();
 
-        if let Some(sync_ids) = rendered_msg.sync_ids_to_delete {
+        if let Some(sync_ids) = rendered_msg.side_effects.sync_ids_to_delete {
             t.execute(
                 &format!("DELETE FROM multi_device_sync WHERE id IN ({sync_ids})"),
                 (),

src/pgp.rs
@@ -21,7 +21,6 @@ use pgp::types::{
 };
 use rand_old::{Rng as _, thread_rng};
 use sha2::Sha256;
-use tokio::runtime::Handle;
 
 use crate::key::{DcKey, Fingerprint};
 
@@ -109,97 +108,93 @@ pub enum SeipdVersion {
 /// Encrypts `plain` text using `public_keys_for_encryption`
 /// and signs it using `private_key_for_signing`.
 #[expect(clippy::arithmetic_side_effects)]
-pub async fn pk_encrypt(
+pub fn pk_encrypt(
     plain: Vec<u8>,
     public_keys_for_encryption: Vec<SignedPublicKey>,
     private_key_for_signing: SignedSecretKey,
     compress: bool,
     seipd_version: SeipdVersion,
 ) -> Result<String> {
-    Handle::current()
-        .spawn_blocking(move || {
-            let mut rng = thread_rng();
+    let mut rng = thread_rng();
 
-            let pkeys = public_keys_for_encryption
-                .iter()
-                .filter_map(select_pk_for_encryption);
-            let subpkts = {
-                let mut hashed = Vec::with_capacity(1 + public_keys_for_encryption.len() + 1);
-                hashed.push(Subpacket::critical(SubpacketData::SignatureCreationTime(
-                    pgp::types::Timestamp::now(),
-                ))?);
-                for key in &public_keys_for_encryption {
-                    let data = SubpacketData::IntendedRecipientFingerprint(key.fingerprint());
-                    let subpkt = match private_key_for_signing.version() < KeyVersion::V6 {
-                        true => Subpacket::regular(data)?,
-                        false => Subpacket::critical(data)?,
-                    };
-                    hashed.push(subpkt);
-                }
-                hashed.push(Subpacket::regular(SubpacketData::IssuerFingerprint(
-                    private_key_for_signing.fingerprint(),
-                ))?);
-                let mut unhashed = vec![];
-                if private_key_for_signing.version() <= KeyVersion::V4 {
-                    unhashed.push(Subpacket::regular(SubpacketData::IssuerKeyId(
-                        private_key_for_signing.legacy_key_id(),
-                    ))?);
-                }
-                SubpacketConfig::UserDefined { hashed, unhashed }
+    let pkeys = public_keys_for_encryption
+        .iter()
+        .filter_map(select_pk_for_encryption);
+    let subpkts = {
+        let mut hashed = Vec::with_capacity(1 + public_keys_for_encryption.len() + 1);
+        hashed.push(Subpacket::critical(SubpacketData::SignatureCreationTime(
+            pgp::types::Timestamp::now(),
+        ))?);
+        for key in &public_keys_for_encryption {
+            let data = SubpacketData::IntendedRecipientFingerprint(key.fingerprint());
+            let subpkt = match private_key_for_signing.version() < KeyVersion::V6 {
+                true => Subpacket::regular(data)?,
+                false => Subpacket::critical(data)?,
             };
+            hashed.push(subpkt);
+        }
+        hashed.push(Subpacket::regular(SubpacketData::IssuerFingerprint(
+            private_key_for_signing.fingerprint(),
+        ))?);
+        let mut unhashed = vec![];
+        if private_key_for_signing.version() <= KeyVersion::V4 {
+            unhashed.push(Subpacket::regular(SubpacketData::IssuerKeyId(
+                private_key_for_signing.legacy_key_id(),
+            ))?);
+        }
+        SubpacketConfig::UserDefined { hashed, unhashed }
+    };
 
-            let msg = MessageBuilder::from_bytes("", plain);
-            let encoded_msg = match seipd_version {
-                SeipdVersion::V1 => {
-                    let mut msg = msg.seipd_v1(&mut rng, SYMMETRIC_KEY_ALGORITHM);
+    let msg = MessageBuilder::from_bytes("", plain);
+    let encoded_msg = match seipd_version {
+        SeipdVersion::V1 => {
+            let mut msg = msg.seipd_v1(&mut rng, SYMMETRIC_KEY_ALGORITHM);
 
-                    for pkey in pkeys {
-                        msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
-                    }
+            for pkey in pkeys {
+                msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
+            }
 
-                    let hash_algorithm = private_key_for_signing.hash_alg();
-                    msg.sign_with_subpackets(
-                        &*private_key_for_signing,
-                        Password::empty(),
-                        hash_algorithm,
-                        subpkts,
-                    );
-                    if compress {
-                        msg.compression(CompressionAlgorithm::ZLIB);
-                    }
+            let hash_algorithm = private_key_for_signing.hash_alg();
+            msg.sign_with_subpackets(
+                &*private_key_for_signing,
+                Password::empty(),
+                hash_algorithm,
+                subpkts,
+            );
+            if compress {
+                msg.compression(CompressionAlgorithm::ZLIB);
+            }
 
-                    msg.to_armored_string(&mut rng, Default::default())?
-                }
-                SeipdVersion::V2 => {
-                    let mut msg = msg.seipd_v2(
-                        &mut rng,
-                        SYMMETRIC_KEY_ALGORITHM,
-                        AeadAlgorithm::Ocb,
-                        ChunkSize::C8KiB,
-                    );
+            msg.to_armored_string(&mut rng, Default::default())?
+        }
+        SeipdVersion::V2 => {
+            let mut msg = msg.seipd_v2(
+                &mut rng,
+                SYMMETRIC_KEY_ALGORITHM,
+                AeadAlgorithm::Ocb,
+                ChunkSize::C8KiB,
+            );
 
-                    for pkey in pkeys {
-                        msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
-                    }
+            for pkey in pkeys {
+                msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
+            }
 
-                    let hash_algorithm = private_key_for_signing.hash_alg();
-                    msg.sign_with_subpackets(
-                        &*private_key_for_signing,
-                        Password::empty(),
-                        hash_algorithm,
-                        subpkts,
-                    );
-                    if compress {
-                        msg.compression(CompressionAlgorithm::ZLIB);
-                    }
+            let hash_algorithm = private_key_for_signing.hash_alg();
+            msg.sign_with_subpackets(
+                &*private_key_for_signing,
+                Password::empty(),
+                hash_algorithm,
+                subpkts,
+            );
+            if compress {
+                msg.compression(CompressionAlgorithm::ZLIB);
+            }
 
-                    msg.to_armored_string(&mut rng, Default::default())?
-                }
-            };
+            msg.to_armored_string(&mut rng, Default::default())?
+        }
+    };
 
-            Ok(encoded_msg)
-        })
-        .await?
+    Ok(encoded_msg)
 }
 
 /// Returns fingerprints
@@ -485,7 +480,7 @@ mod tests {
         config::Config,
         decrypt,
         key::{load_self_public_key, self_fingerprint, store_self_keypair},
-        mimefactory::{render_outer_message, wrap_encrypted_part},
+        mimefactory::{part_to_vec, wrap_encrypted_part},
         test_utils::{TestContext, TestContextManager, alice_keypair, bob_keypair},
         token,
     };
@@ -511,8 +506,8 @@ mod tests {
         store_self_keypair(t, secret_key).await?;
 
         let mime_message = wrap_encrypted_part(bytes.try_into().unwrap());
-        let rendered = render_outer_message(vec![], mime_message);
-        let parsed = mailparse::parse_mail(rendered.as_bytes())?;
+        let rendered = part_to_vec(mime_message);
+        let parsed = mailparse::parse_mail(&rendered)?;
         let (decrypted, _fp) = decrypt::decrypt(t, &parsed).await?.unwrap();
         Ok(decrypted)
     }
@@ -585,7 +580,6 @@ mod tests {
                     compress,
                     SeipdVersion::V2,
                 )
-                .await
                 .unwrap()
             })
             .await
@@ -776,8 +770,7 @@ mod tests {
             KEYS.alice_secret.clone(),
             compress,
             SeipdVersion::V2,
-        )
-        .await?;
+        )?;
 
         // Trying to decrypt it should fail with an OK error message:
         let bob_private_keyring = crate::key::load_self_secret_keyring(bob).await?;

chatmail/core

src/chat.rs
@@ -35,11 +35,13 @@ use crate::download::{
 use crate::ensure_and_debug_assert_eq;
 use crate::ephemeral::{Timer as EphemeralTimer, start_chat_ephemeral_timers};
 use crate::events::EventType;
+use crate::key;
 use crate::key::{Fingerprint, self_fingerprint};
 use crate::location;
 use crate::log::{LogExt, warn};
 use crate::logged_debug_assert;
 use crate::message::{self, Message, MessageState, MsgId, Viewtype};
+use crate::mimefactory;
 use crate::mimefactory::{MimeFactory, RenderedEmail};
 use crate::mimeparser::SystemMessage;
 use crate::param::{Param, Params};
@@ -2756,6 +2758,11 @@ async fn render_mime_message_and_pre_message(
     msg: &mut Message,
     mimefactory: MimeFactory,
 ) -> Result<(Option<RenderedEmail>, RenderedEmail)> {
+    let from_addr = context.get_primary_self_addr().await?;
+    let public_key = key::load_self_public_key(context).await?;
+    let secret_key = key::load_self_secret_key(context).await?;
+    let timestamp = msg.timestamp_sort;
+
     let needs_pre_message = msg.viewtype.has_file()
         && mimefactory.will_be_encrypted() // unencrypted is likely email, we don't want to spam by sending multiple messages
         && msg
@@ -2772,15 +2779,33 @@ async fn render_mime_message_and_pre_message(
 
         let mut mimefactory_post_msg = mimefactory.clone();
         mimefactory_post_msg.set_as_post_message();
-        let rendered_msg = Box::pin(mimefactory_post_msg.render(context))
+        let (queued_msg, side_effects) = Box::pin(mimefactory_post_msg.into_queued_mail(context))
             .await
             .context("Failed to render post-message")?;
 
+        let rendered_msg = mimefactory::render_queued_mail(
+            queued_msg,
+            &public_key,
+            &secret_key,
+            from_addr.clone(),
+            timestamp,
+            side_effects,
+        )?;
+
         let mut mimefactory_pre_msg = mimefactory;
         mimefactory_pre_msg.set_as_pre_message_for(&rendered_msg);
-        let rendered_pre_msg = Box::pin(mimefactory_pre_msg.render(context))
-            .await
-            .context("pre-message failed to render")?;
+        let (queued_pre_msg, pre_side_effects) =
+            Box::pin(mimefactory_pre_msg.into_queued_mail(context))
+                .await
+                .context("pre-message failed to render")?;
+        let rendered_pre_msg = mimefactory::render_queued_mail(
+            queued_pre_msg,
+            &public_key,
+            &secret_key,
+            from_addr,
+            timestamp,
+            pre_side_effects,
+        )?;
 
         if rendered_pre_msg.message.len() > PRE_MSG_SIZE_WARNING_THRESHOLD {
             warn!(
@@ -2793,7 +2818,17 @@ async fn render_mime_message_and_pre_message(
 
         Ok((Some(rendered_pre_msg), rendered_msg))
     } else {
-        Ok((None, Box::pin(mimefactory.render(context)).await?))
+        let (queued_msg, side_effects) = Box::pin(mimefactory.into_queued_mail(context)).await?;
+        let rendered_msg = mimefactory::render_queued_mail(
+            queued_msg,
+            &public_key,
+            &secret_key,
+            from_addr,
+            timestamp,
+            side_effects,
+        )?;
+
+        Ok((None, rendered_msg))
     }
 }
 
@@ -2838,7 +2873,6 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
             return Err(err);
         }
     };
-    let attach_selfavatar = mimefactory.attach_selfavatar;
     let mut recipients = mimefactory.recipients();
 
     let from = context.get_primary_self_addr().await?;
@@ -2925,14 +2959,22 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
 
     let now = time();
 
-    if let Some(last_added_location_timestamp) = rendered_msg.last_added_location_timestamp {
+    if let Some(last_added_location_timestamp) =
+        rendered_msg.side_effects.last_added_location_timestamp
+    {
         location::set_kml_sent_timestamp(context, msg.chat_id, last_added_location_timestamp)
             .await?;
     }
 
-    if attach_selfavatar && let Err(err) = msg.chat_id.set_selfavatar_timestamp(context, now).await
+    if rendered_msg.side_effects.avatar_is_attached
+        || rendered_pre_msg
+            .as_ref()
+            .is_some_and(|msg| msg.side_effects.avatar_is_attached)
     {
-        error!(context, "Failed to set selfavatar timestamp: {err:#}.");
+        msg.chat_id
+            .set_selfavatar_timestamp(context, now)
+            .await
+            .context("Failed to set selfavatar timestamp")?;
     }
 
     if rendered_msg.is_encrypted {
@@ -2940,7 +2982,7 @@ pub(crate) async fn create_send_msg_jobs(context: &Context, msg: &mut Message) -
     } else {
         msg.param.remove(Param::GuaranteeE2ee);
     }
-    msg.subject.clone_from(&rendered_msg.subject);
+    msg.subject.clone_from(&rendered_msg.side_effects.subject);
     // Sort the message to the bottom. Employ `msgs_index7` to compute `timestamp`.
     context
         .sql
@@ -2972,7 +3014,7 @@ WHERE id=?
     let trans_fn = |t: &mut rusqlite::Transaction| {
         let mut row_ids = Vec::<i64>::new();
 
-        if let Some(sync_ids) = rendered_msg.sync_ids_to_delete {
+        if let Some(sync_ids) = rendered_msg.side_effects.sync_ids_to_delete {
             t.execute(
                 &format!("DELETE FROM multi_device_sync WHERE id IN ({sync_ids})"),
                 (),

src/pgp.rs
@@ -21,7 +21,6 @@ use pgp::types::{
 };
 use rand_old::{Rng as _, thread_rng};
 use sha2::Sha256;
-use tokio::runtime::Handle;
 
 use crate::key::{DcKey, Fingerprint};
 
@@ -109,97 +108,93 @@ pub enum SeipdVersion {
 /// Encrypts `plain` text using `public_keys_for_encryption`
 /// and signs it using `private_key_for_signing`.
 #[expect(clippy::arithmetic_side_effects)]
-pub async fn pk_encrypt(
+pub fn pk_encrypt(
     plain: Vec<u8>,
     public_keys_for_encryption: Vec<SignedPublicKey>,
     private_key_for_signing: SignedSecretKey,
     compress: bool,
     seipd_version: SeipdVersion,
 ) -> Result<String> {
-    Handle::current()
-        .spawn_blocking(move || {
-            let mut rng = thread_rng();
+    let mut rng = thread_rng();
 
-            let pkeys = public_keys_for_encryption
-                .iter()
-                .filter_map(select_pk_for_encryption);
-            let subpkts = {
-                let mut hashed = Vec::with_capacity(1 + public_keys_for_encryption.len() + 1);
-                hashed.push(Subpacket::critical(SubpacketData::SignatureCreationTime(
-                    pgp::types::Timestamp::now(),
-                ))?);
-                for key in &public_keys_for_encryption {
-                    let data = SubpacketData::IntendedRecipientFingerprint(key.fingerprint());
-                    let subpkt = match private_key_for_signing.version() < KeyVersion::V6 {
-                        true => Subpacket::regular(data)?,
-                        false => Subpacket::critical(data)?,
-                    };
-                    hashed.push(subpkt);
-                }
-                hashed.push(Subpacket::regular(SubpacketData::IssuerFingerprint(
-                    private_key_for_signing.fingerprint(),
-                ))?);
-                let mut unhashed = vec![];
-                if private_key_for_signing.version() <= KeyVersion::V4 {
-                    unhashed.push(Subpacket::regular(SubpacketData::IssuerKeyId(
-                        private_key_for_signing.legacy_key_id(),
-                    ))?);
-                }
-                SubpacketConfig::UserDefined { hashed, unhashed }
+    let pkeys = public_keys_for_encryption
+        .iter()
+        .filter_map(select_pk_for_encryption);
+    let subpkts = {
+        let mut hashed = Vec::with_capacity(1 + public_keys_for_encryption.len() + 1);
+        hashed.push(Subpacket::critical(SubpacketData::SignatureCreationTime(
+            pgp::types::Timestamp::now(),
+        ))?);
+        for key in &public_keys_for_encryption {
+            let data = SubpacketData::IntendedRecipientFingerprint(key.fingerprint());
+            let subpkt = match private_key_for_signing.version() < KeyVersion::V6 {
+                true => Subpacket::regular(data)?,
+                false => Subpacket::critical(data)?,
             };
+            hashed.push(subpkt);
+        }
+        hashed.push(Subpacket::regular(SubpacketData::IssuerFingerprint(
+            private_key_for_signing.fingerprint(),
+        ))?);
+        let mut unhashed = vec![];
+        if private_key_for_signing.version() <= KeyVersion::V4 {
+            unhashed.push(Subpacket::regular(SubpacketData::IssuerKeyId(
+                private_key_for_signing.legacy_key_id(),
+            ))?);
+        }
+        SubpacketConfig::UserDefined { hashed, unhashed }
+    };
 
-            let msg = MessageBuilder::from_bytes("", plain);
-            let encoded_msg = match seipd_version {
-                SeipdVersion::V1 => {
-                    let mut msg = msg.seipd_v1(&mut rng, SYMMETRIC_KEY_ALGORITHM);
+    let msg = MessageBuilder::from_bytes("", plain);
+    let encoded_msg = match seipd_version {
+        SeipdVersion::V1 => {
+            let mut msg = msg.seipd_v1(&mut rng, SYMMETRIC_KEY_ALGORITHM);
 
-                    for pkey in pkeys {
-                        msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
-                    }
+            for pkey in pkeys {
+                msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
+            }
 
-                    let hash_algorithm = private_key_for_signing.hash_alg();
-                    msg.sign_with_subpackets(
-                        &*private_key_for_signing,
-                        Password::empty(),
-                        hash_algorithm,
-                        subpkts,
-                    );
-                    if compress {
-                        msg.compression(CompressionAlgorithm::ZLIB);
-                    }
+            let hash_algorithm = private_key_for_signing.hash_alg();
+            msg.sign_with_subpackets(
+                &*private_key_for_signing,
+                Password::empty(),
+                hash_algorithm,
+                subpkts,
+            );
+            if compress {
+                msg.compression(CompressionAlgorithm::ZLIB);
+            }
 
-                    msg.to_armored_string(&mut rng, Default::default())?
-                }
-                SeipdVersion::V2 => {
-                    let mut msg = msg.seipd_v2(
-                        &mut rng,
-                        SYMMETRIC_KEY_ALGORITHM,
-                        AeadAlgorithm::Ocb,
-                        ChunkSize::C8KiB,
-                    );
+            msg.to_armored_string(&mut rng, Default::default())?
+        }
+        SeipdVersion::V2 => {
+            let mut msg = msg.seipd_v2(
+                &mut rng,
+                SYMMETRIC_KEY_ALGORITHM,
+                AeadAlgorithm::Ocb,
+                ChunkSize::C8KiB,
+            );
 
-                    for pkey in pkeys {
-                        msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
-                    }
+            for pkey in pkeys {
+                msg.encrypt_to_key_anonymous(&mut rng, &pkey)?;
+            }
 
-                    let hash_algorithm = private_key_for_signing.hash_alg();
-                    msg.sign_with_subpackets(
-                        &*private_key_for_signing,
-                        Password::empty(),
-                        hash_algorithm,
-                        subpkts,
-                    );
-                    if compress {
-                        msg.compression(CompressionAlgorithm::ZLIB);
-                    }
+            let hash_algorithm = private_key_for_signing.hash_alg();
+            msg.sign_with_subpackets(
+                &*private_key_for_signing,
+                Password::empty(),
+                hash_algorithm,
+                subpkts,
+            );
+            if compress {
+                msg.compression(CompressionAlgorithm::ZLIB);
+            }
 
-                    msg.to_armored_string(&mut rng, Default::default())?
-                }
-            };
+            msg.to_armored_string(&mut rng, Default::default())?
+        }
+    };
 
-            Ok(encoded_msg)
-        })
-        .await?
+    Ok(encoded_msg)
 }
 
 /// Returns fingerprints
@@ -485,7 +480,7 @@ mod tests {
         config::Config,
         decrypt,
         key::{load_self_public_key, self_fingerprint, store_self_keypair},
-        mimefactory::{render_outer_message, wrap_encrypted_part},
+        mimefactory::{part_to_bytes, wrap_encrypted_part},
         test_utils::{TestContext, TestContextManager, alice_keypair, bob_keypair},
         token,
     };
@@ -511,8 +506,8 @@ mod tests {
         store_self_keypair(t, secret_key).await?;
 
         let mime_message = wrap_encrypted_part(bytes.try_into().unwrap());
-        let rendered = render_outer_message(vec![], mime_message);
-        let parsed = mailparse::parse_mail(rendered.as_bytes())?;
+        let rendered = part_to_bytes(mime_message);
+        let parsed = mailparse::parse_mail(&rendered)?;
         let (decrypted, _fp) = decrypt::decrypt(t, &parsed).await?.unwrap();
         Ok(decrypted)
     }
@@ -585,7 +580,6 @@ mod tests {
                     compress,
                     SeipdVersion::V2,
                 )
-                .await
                 .unwrap()
             })
             .await
@@ -776,8 +770,7 @@ mod tests {
             KEYS.alice_secret.clone(),
             compress,
             SeipdVersion::V2,
-        )
-        .await?;
+        )?;
 
         // Trying to decrypt it should fail with an OK error message:
         let bob_private_keyring = crate::key::load_self_secret_keyring(bob).await?;