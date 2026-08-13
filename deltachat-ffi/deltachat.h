eada85dd99db224843ae63873325f3a2b69c1897
biørn <r10s@b44t.com>
2026-08-13 16:44:54 +0200

    Update deltachat-ffi/deltachat.h
    Co-authored-by: adb <adb@merlinux.eu>

--- deltachat-ffi/deltachat.h
+++ deltachat-ffi/deltachat.h
@@ -3557,7 +3557,7 @@ dc_lot_t*        dc_chatlist_get_summary2    (dc_context_t* context, uint32_t ch
 /**
  * Get info summary for a chat, in JSON format.
  *
- * @deprecated 2026-08-13, use dedicatged dc_chat_get_*() getters or jsonrpc
+ * @deprecated 2026-08-13, use dedicated dc_chat_get_*() getters or jsonrpc
  *
  * The returned JSON string has the following key/values:
  *
