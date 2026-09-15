06150d6dc20d91f870728e55e6a47ba0f5d054d7
B. Petersen <r10s@b44t.com>
2026-09-15 15:29:19 +0200

    docs: remove protect_autocrypt setting

--- deltachat-ffi/deltachat.h
+++ deltachat-ffi/deltachat.h
@@ -453,11 +453,6 @@ char*           dc_get_blobdir               (const dc_context_t* context);
  *                    always auto-downloaded.
  *                    0 = no limit (default).
  *                    Changes affect future messages only.
- * - `protect_autocrypt` = Enable Header Protection for Autocrypt header.
- *                    This is an experimental option not compatible to other MUAs
- *                    and older Delta Chat versions.
- *                    1 = enable.
- *                    0 = disable (default).
  * - `gossip_period` = How often to gossip Autocrypt keys in chats with multiple recipients, in
  *                    seconds. 2 days by default.
  *                    This is not supposed to be changed by UIs and only used for testing.
