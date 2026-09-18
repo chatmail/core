36742637120e747df65720967ce988e884462337
link2xt <link2xt@testrun.org>
2026-09-18 17:35:50 +0000

    api!: remove Contact.get_name_n_addr() and related APIs
    UIs should use display name everywhere and avoid displaying email addresses.
    
    BREAKING CHANGE: dc_contact_get_name_n_addr() CFFI is removed
    BREAKING CHANGE: JSON-RPC contact objects don't have nameAndAddr field anymore

--- deltachat-repl/src/cmdline.rs
+++ deltachat-repl/src/cmdline.rs
@@ -1113,11 +1113,11 @@ pub async fn cmdline(context: Context, line: &str, chat_id: &mut ChatId) -> Resu
 
             let contact_id = ContactId::new(arg1.parse()?);
             let contact = Contact::get_by_id(&context, contact_id).await?;
-            let name_n_addr = contact.get_name_n_addr();
+            let name = contact.get_display_name();
+            let addr = contact.get_addr();
 
             let mut res = format!(
-                "Contact info for: {}:\nIcon: {}\n",
-                name_n_addr,
+                "Contact info for: {name} ({addr}):\nIcon: {}\n",
                 match contact.get_profile_image(&context).await? {
                     Some(image) => image.to_str().unwrap().to_string(),
                     None => "NoIcon".to_string(),
