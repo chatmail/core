8858c01f3f2365957b0f602b69c7b53ae7576f90
Hocuri <hocuri@gmx.de>
2026-09-22 13:33:55 +0200

    feat: Don't show "member added" messages in channels (#8730)
    Fix https://github.com/chatmail/core/issues/8606
    
    "Member added" messages in channels clutter the UI because sometimes, a
    lot of recipients join, and it is not that interesting for the channel
    owner who joined when exactly. Also, the correct string would be "XY
    subscribed to your channel", but it seems easier and better to just
    remove these messages.
    
    There are 3 places in the code that can add a member to a channel:
    - `add_contact_to_chat_ext()`, which is called during securejoin
    - Seeing a member-addition message from our own second device in
    receive_imf
    - A sync message telling us about a member addition which we missed

--- src/receive_imf.rs
+++ src/receive_imf.rs
@@ -3733,10 +3733,9 @@ async fn apply_out_broadcast_changes(
         if from_id == ContactId::SELF {
             let added_id = lookup_key_contact_by_fingerprint(context, added_fpr).await?;
             if let Some(added_id) = added_id {
-                if chat::is_contact_in_chat(context, chat.id, added_id).await? {
-                    info!(context, "No-op broadcast addition (TRASH)");
-                    better_msg.get_or_insert("".to_string());
-                } else {
+                info!(context, "Broadcast addition (TRASH)");
+                better_msg.get_or_insert("".to_string());
+                if !chat::is_contact_in_chat(context, chat.id, added_id).await? {
                     chat::add_to_chat_contacts_table(
                         context,
                         mime_parser.timestamp_sent,
@@ -3744,11 +3743,6 @@ async fn apply_out_broadcast_changes(
                         &[added_id],
                     )
                     .await?;
-                    let msg =
-                        stock_str::msg_add_member_local(context, added_id, ContactId::UNDEFINED)
-                            .await;
-                    better_msg.get_or_insert(msg);
-                    added_removed_id = Some(added_id);
                     send_event_chat_modified = true;
                 }
             } else {
