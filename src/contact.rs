88754817a589711aef992131a16a12255d9891e7
B. Petersen <r10s@b44t.com>
2026-09-22 13:17:23 +0200

    fix: blocked or email contacts contacts are never recently seen or old

--- src/contact.rs
+++ src/contact.rs
@@ -751,7 +751,7 @@ impl Contact {
 
     /// Returns `true` if this contact was seen recently.
     pub fn get_freshness(&self) -> Freshness {
-        if self.id.is_special() {
+        if self.id.is_special() || !self.is_key_contact() || self.is_blocked() {
             return Freshness::Normal;
         }
 
